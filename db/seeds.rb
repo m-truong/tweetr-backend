222f90cb2377b9fe7e9eb8b19cd4873748cb292f
Amanda Hayes <amandahayes@Amandas-MacBook-Pro.local>
2021-01-21 21:24:33 -0800

    Adding seeds file back in

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,27 @@ require 'faker'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-2.times do
+10.times do |i|
     Tweet.create({
         author: Faker::Twitter.screen_name,
         title: Faker::Twitter.status[:text],
         content: Faker::Twitter.status[:text]
     })
-end
+    Reply.create({
+        content: Faker::Twitter.status[:text],
+        author: Faker::Twitter.screen_name,
+        tweet_id: i+1
+    })
+    Reply.create({
+        content: Faker::Twitter.status[:text],
+        author: Faker::Twitter.screen_name,
+        tweet_id: i+1
+    })
+    Reply.create({
+        content: Faker::Twitter.status[:text],
+        author: Faker::Twitter.screen_name,
+        tweet_id: i+1
+    })
 
 
 # Tweet.create({
